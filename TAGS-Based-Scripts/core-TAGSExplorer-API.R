7e140b151dfcb3ca0873bce89b0cd10da0cacb38
psychemedia <tony.hirst@gmail.com>
2012-02-01 15:19:00 +0000

    Add RCurl library dependency

--- TAGS-Based-Scripts/core-TAGSExplorer-API.R
+++ TAGS-Based-Scripts/core-TAGSExplorer-API.R
@@ -1,6 +1,9 @@
 ## Core functions for grabbing data from a TAGSExplorer spreadsheet
 ## I think we can gain access to this from an arbitrary script via something like:
 ## source("TAGS-Based-Scripts/core-TAGSExplorer-API.R")
+
+require(RCurl)
+
 gsqAPI = function(key,query,gid=0){
   url=paste( sep="",'http://spreadsheets.google.com/tq?', 'tqx=out:csv','&tq=', curlEscape(query), '&key=', key, '&gid=', gid)
   print(paste("Trying to load file: ",url))

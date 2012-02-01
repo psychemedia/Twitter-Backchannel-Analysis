48ea7859b21872ffd48bc06135d2001584ee0786
psychemedia <tony.hirst@gmail.com>
2012-02-01 15:14:04 +0000

    Should we be loading common functions from a common, emerging set of library functions?

---
+++ TAGS-Based-Scripts/core-TAGSExplorer-API.R
@@ -0,0 +1,8 @@
+## Core functions for grabbing data from a TAGSExplorer spreadsheet
+## I think we can gain access to this from an arbitrary script via something like:
+## source("TAGS-Based-Scripts/core-TAGSExplorer-API.R")
+gsqAPI = function(key,query,gid=0){
+  url=paste( sep="",'http://spreadsheets.google.com/tq?', 'tqx=out:csv','&tq=', curlEscape(query), '&key=', key, '&gid=', gid)
+  print(paste("Trying to load file: ",url))
+  return( read.csv( url ) ) 
+}

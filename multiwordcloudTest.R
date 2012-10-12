689900ed69d9174b2696fd0532f3e7db4e3329ae
Tony Hirst <tony.hirst@gmail.com>
2012-10-12 21:23:05 +0100

    Tidying..

--- multiwordcloudTest.R
+++ multiwordcloudTest.R
@@ -1,34 +1,49 @@
+#DEPENDENCIES
+require(tm)
+require(plyr)
+require(wordcloud)
+source('doodles/wordcloud.R')
 
 
+xlist=c('milton','keynes')
+
+stub='TEDxMK'
 #wordcloud stuff form wordcloud.R
 mk1=subset(mk,Modularity.Class==1)
-require(tm)
-require(plyr)
 
 gc=function(x) generateCorpus(x$desc)
-wcg=wordcloud.generate(gc(x))
-gc2=function(x) generateCorpus(x$desc,c('milton','keynes'))
-wcg2=function(x) wordcloud.generate(gc2(x))
-wcg3=function(x) wordcloud.generate(gc2(x),3)
 
-mk2=mk
-mk2=mk2[with(mk2, order(Modularity.Class)), ]
-View(mk2)
+gc2=function(x) generateCorpus(x$desc,xlist)
+wcg2=function(x) wordcloud.generate(gc2(x))
+wcg3=function(x) wordcloud.generate(gc2(x),4)
+wcg4=function(x) {
+  fn=paste('./output/',stub,'tmp',x$Modularity.Class,'.svg',sep='')
+  svg(fn)
+  wcg3(x)
+  dev.off()
+}
+  
+wcg=wordcloud.generate(gc(mk1))
+
+xlist=c('')
+wcdata=comact4
+
+wcdata=subset(ou,Eigenvector.Centrality>0)
+
+wcdata=comact4
+stub='comact4'
+wcdata=wcdata[with(wcdata, order(Modularity.Class)), ]
+ddply(wcdata,.(Modularity.Class),wcg4)
+ddply(wcdata,.(Modularity.Class),wcg3)
+
 ddply(mk,.(Modularity.Class),wcg3)
-require(plyr)
-ddply(mk,.(Modularity.Class),wcg3)
-ddply(mk,.(Modularity.Class),wcg3)
-reqiore(tm)
-require(tm)
-ddply(mk,.(Modularity.Class),wcg3)
-tw.corpus= Corpus(VectorSource(mk))
-tw.corpus
+
+ddply(mk,.(Modularity.Class),wcg4)
+
 tw.corpus= Corpus(VectorSource(mk$desc))
 tw.corpus
 tw.corpus?png= Corpus(VectorSource(mk$desc))
-?png
-png
-?svg
-svg("tmp.svg")
+
+svg("output/tmp.svg")
 wordcloud.generate(generateCorpus(mk))
 dev.off()
